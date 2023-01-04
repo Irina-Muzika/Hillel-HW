2c38e5a233f24b665ea62fd88942552d82d2a62e
Irina-Muzika <106766375+Irina-Muzika@users.noreply.github.com>
2023-01-04 12:30:25 +0200

    add validation

--- 29-lesson/src/Form.js
+++ 29-lesson/src/Form.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import styles from './Form.module.css'
 
 function Form({ onSubmit, todoList, todo, setTodo }) {
     const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+
 
     useEffect(() => {
-        if(todo.id){
+        if (todo.id) {
             setMessage(todo.title)
         }
     }, [todo])
@@ -18,25 +21,30 @@ function Form({ onSubmit, todoList, todo, setTodo }) {
             done: false,
             ...currentTodo,
             title: message,
-        } 
+        }
+
+        if (message.trim() === '') {
+            setError('input value is not empty')
+            return
+        }
 
         onSubmit(newTodo)
 
         setMessage('')
 
         setTodo('')
+
+        setError('')
     }
 
     function onMessageChange(e) {
-
-      
         setMessage(e.target.value)
-       
     }
 
     return (
         <form onSubmit={onFormSubmit}>
             <input type="text" value={message} onChange={onMessageChange} />
+            {error ? <p className={styles.error}>{error}</p> : null}
         </form>
     );
 }
